12a24f87273ddbe88eb0cd1981c6c70bbe1ca2fb
Bryan <bryanagan25@gmail.com>
2025-01-28 17:12:37 +0800

    Add fail_to_add dialog, it shows up when the application finds one or more issues upon adding a student

---
+++ src/add_item_state/fail_to_add_item.py
@@ -0,0 +1,34 @@
+from PyQt6.QtWidgets import QDialog
+from PyQt6.QtCore import QSize
+
+from add_item_state.fail_to_add_item_design import Ui_Dialog as FailToAddItemUI
+
+
+class FailToAddItemDialog(QDialog, FailToAddItemUI):
+    def __init__(self, issues):
+        super().__init__()
+
+        self.setupUi(self)
+
+        self.issues = issues
+
+        self.pushButton.clicked.connect(self.close_dialog)
+
+        self.load_issues()
+
+    def load_issues(self):
+        issues_str = '\n'.join(self.issues)
+
+        additional_space = (len(self.issues) - 1) * 15
+
+        print(issues_str)
+
+        self.label_2.setText(issues_str)
+
+        self.setMinimumHeight(self.height() + additional_space)
+        self.setMaximumHeight(self.height())
+
+
+
+    def close_dialog(self):
+        self.close()
--- src/student/add_student.py
+++ src/student/add_student.py
@@ -1,6 +1,9 @@
 from PyQt6.QtWidgets import QDialog
 
 from student.add_student_design import Ui_Dialog as AddStudentUI
+from add_item_state.fail_to_add_item import FailToAddItemDialog
+
+import re
 
 
 class AddStudentDialog(QDialog, AddStudentUI):
@@ -9,7 +12,52 @@ class AddStudentDialog(QDialog, AddStudentUI):
 
         self.setupUi(self)
 
-        self.pushButton.clicked.connect(self.add_student_to_csv)
+        self.add_student_button.clicked.connect(self.add_student_to_csv)
 
     def add_student_to_csv(self):
-        print("Student has been added")
+
+        issues = []
+
+        if (self.id_number_lineedit.text()).strip() == "":
+            issues.append("ID Number is blank")
+        elif not self.is_valid_id_number():
+            issues.append("ID Number is not in the correct format")
+
+        if (self.first_name_lineedit.text()).strip() == "":
+            issues.append("First name is blank")
+        elif not self.is_valid_first_name():
+            issues.append("First name is not in the correct format")
+
+        if (self.last_name_lineedit.text()).strip() == "":
+            issues.append("Last name is blank")
+        elif not self.is_valid_last_name():
+            issues.append("Last name is not in the correct format")
+
+        if (self.program_code_lineedit.text()).strip() == "":
+            issues.append("Program code is blank")
+        elif not self.is_valid_program_code():
+            issues.append("Program code is not in the correct format")
+
+        if issues:
+            self.fail_to_add_item_dialog = FailToAddItemDialog(issues)
+            self.fail_to_add_item_dialog.show()
+
+    def is_valid_id_number(self):
+        valid_id_number = re.match(r'^[0-9]{4}-[0-9]{4}$', self.id_number_lineedit.text())
+
+        return True if valid_id_number else False
+
+    def is_valid_first_name(self):
+        valid_first_name = re.match(r'^[a-zA-Z ]+$', self.first_name_lineedit.text())
+
+        return True if valid_first_name else False
+
+    def is_valid_last_name(self):
+        valid_last_name = re.match(r'^[a-zA-Z ]+$', self.last_name_lineedit.text())
+
+        return True if valid_last_name else False
+
+    def is_valid_program_code(self):
+        valid_program_code = re.match(r'^[A-Z]{3,}$', self.program_code_lineedit.text())
+
+        return True if valid_program_code else False
