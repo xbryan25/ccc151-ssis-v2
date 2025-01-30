e71800c0392feb867b2f66886fff826f3270e2b8
Bryan <bryanagan25@gmail.com>
2025-01-30 08:49:02 +0800

    Connect add_student_to_csv() to students.csv

--- src/students/add_student.py
+++ src/students/add_student.py
@@ -4,6 +4,7 @@ from students.add_student_design import Ui_Dialog as AddStudentUI
 from add_item_state.fail_to_add_item import FailToAddItemDialog
 
 import re
+import csv
 
 
 class AddStudentDialog(QDialog, AddStudentUI):
@@ -16,12 +17,15 @@ class AddStudentDialog(QDialog, AddStudentUI):
 
     def add_student_to_csv(self):
 
+        students_information = self.get_existing_students_information()
         issues = []
 
         if (self.id_number_lineedit.text()).strip() == "":
             issues.append("ID Number is blank")
         elif not self.is_valid_id_number():
             issues.append("ID Number is not in the correct format")
+        elif (self.id_number_lineedit.text()).strip() in students_information["ID Number"]:
+            issues.append("ID Number has already been taken")
 
         if (self.first_name_lineedit.text()).strip() == "":
             issues.append("First name is blank")
@@ -33,6 +37,10 @@ class AddStudentDialog(QDialog, AddStudentUI):
         elif not self.is_valid_last_name():
             issues.append("Last name is not in the correct format")
 
+        full_name = f"{(self.first_name_lineedit.text()).strip()} {(self.last_name_lineedit.text()).strip()}"
+        if full_name in students_information["Full Name"]:
+            issues.append("Name combination already exists")
+
         if (self.program_code_lineedit.text()).strip() == "":
             issues.append("Program code is blank")
         elif not self.is_valid_program_code():
@@ -41,6 +49,31 @@ class AddStudentDialog(QDialog, AddStudentUI):
         if issues:
             self.fail_to_add_item_dialog = FailToAddItemDialog(issues)
             self.fail_to_add_item_dialog.show()
+        else:
+            student_to_add = [self.id_number_lineedit.text(),
+                              self.first_name_lineedit.text(),
+                              self.last_name_lineedit.text(),
+                              self.year_level_combobox.currentText(),
+                              self.gender_combobox.currentText(),
+                              self.program_code_lineedit.text()]
+
+            with open("databases/students.csv", 'a', newline='') as from_students_csv:
+                writer = csv.writer(from_students_csv)
+
+                writer.writerow(student_to_add)
+                print("Success")
+
+    def get_existing_students_information(self):
+        students_information = {"ID Number": [], "Full Name": []}
+
+        with open("databases/students.csv", 'r') as from_students_csv:
+            reader = csv.reader(from_students_csv)
+
+            for row in reader:
+                students_information["ID Number"].append(row[0])
+                students_information["Full Name"].append(f"{row[1].strip()} {row[2].strip()}")
+
+        return students_information
 
     def is_valid_id_number(self):
         valid_id_number = re.match(r'^[0-9]{4}-[0-9]{4}$', self.id_number_lineedit.text())
