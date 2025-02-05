fb5dbb7c1670d5e2eb2b669529b33ee66a36c474
Bryan <bryanagan25@gmail.com>
2025-02-05 08:46:21 +0800

    Add edit item state dialogs, minimal differences from add item state dialogs

---
+++ src/helper_dialogs/edit_item_state/fail_to_edit_item.py
@@ -0,0 +1,42 @@
+from PyQt6.QtWidgets import QDialog
+from PyQt6.QtCore import QSize
+
+from helper_dialogs.edit_item_state.fail_to_edit_item_design import Ui_Dialog as FailToEditItemUI
+
+
+class FailToEditItemDialog(QDialog, FailToEditItemUI):
+    def __init__(self, issues, information_type):
+        super().__init__()
+
+        self.setupUi(self)
+
+        self.issues = issues
+        self.information_type = information_type
+
+        self.pushButton.clicked.connect(self.close_dialog)
+
+        self.load_issues()
+        self.edit_window_title()
+
+    def load_issues(self):
+        issues_str = '\n'.join(self.issues)
+
+        additional_space = (len(self.issues) - 1) * 15
+
+        print(issues_str)
+
+        self.label_2.setText(issues_str)
+
+        self.setMinimumHeight(self.height() + additional_space)
+        self.setMaximumHeight(self.height())
+
+    def edit_window_title(self):
+        if self.information_type == "student":
+            self.setWindowTitle("Fail to edit student")
+        elif self.information_type == "program":
+            self.setWindowTitle("Fail to edit program")
+        elif self.information_type == "college":
+            self.setWindowTitle("Fail to edit college")
+
+    def close_dialog(self):
+        self.close()
---
+++ src/helper_dialogs/edit_item_state/success_edit_item.py
@@ -0,0 +1,32 @@
+from PyQt6.QtWidgets import QDialog
+from PyQt6.QtCore import QSize
+
+from helper_dialogs.edit_item_state.success_edit_item_design import Ui_Dialog as SuccessEditItemUI
+
+
+class SuccessEditItemDialog(QDialog, SuccessEditItemUI):
+    def __init__(self, edit_item_type, edit_item_dialog=None):
+        super().__init__()
+
+        self.setupUi(self)
+
+        self.edit_item_type = edit_item_type
+        self.edit_item_dialog = edit_item_dialog
+
+        self.proceed_button.clicked.connect(self.close_dialog)
+
+        self.edit_message()
+
+    def edit_message(self):
+        if self.edit_item_type == "student":
+            self.message_label.setText("No issues found when editing the student")
+        elif self.edit_item_type == "program":
+            self.message_label.setText("No issues found when editing the program")
+        elif self.edit_item_type == "college":
+            self.message_label.setText("No issues found when editing the college")
+
+    def close_dialog(self):
+        if self.edit_item_dialog:
+            self.edit_item_dialog.close()
+
+        self.close()
