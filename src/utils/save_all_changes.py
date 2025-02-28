a3e5c40b169bc89aae83d7576aba545edc79a6de
Bryan <bryanagan25@gmail.com>
2025-02-28 23:28:25 +0800

    Fix tab navigation on input dialogs, and model not getting saved properly when it is empty

--- src/utils/save_all_changes.py
+++ src/utils/save_all_changes.py
@@ -8,17 +8,17 @@ class SaveAllChanges:
         self.programs_table_model = programs_table_model
         self.colleges_table_model = colleges_table_model
 
-        if not self.students_table_model or not self.students_table_model.get_data():
+        if not self.students_table_model or self.students_table_model.get_data()[0][0] == "":
             self.students_table_model_data = []
         else:
             self.students_table_model_data = self.students_table_model.get_data()
 
-        if not self.programs_table_model or not programs_table_model.get_data():
+        if not self.programs_table_model or programs_table_model.get_data()[0][0] == "":
             self.programs_table_model_data = []
         else:
             self.programs_table_model_data = self.programs_table_model.get_data()
 
-        if not self.colleges_table_model or not colleges_table_model.get_data():
+        if not self.colleges_table_model or colleges_table_model.get_data()[0][0] == "":
             self.colleges_table_model_data = []
         else:
             self.colleges_table_model_data = self.colleges_table_model.get_data()
