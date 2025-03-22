cbcec72a2cee94e1900a8f8ff1eeea82f957ef00
Bryan <bryanagan25@gmail.com>
2025-03-22 20:40:39 +0800

    Used pymysql for MySQL connection, migrated getting data from csv to getting data from MySQL database

--- src/driver.py
+++ src/driver.py
@@ -21,6 +21,5 @@ def main():
     application_window.show()
     app.exec()
 
-
 if __name__ == "__main__":
     main()
--- src/utils/load_information_from_database.py
+++ src/utils/load_information_from_database.py
@@ -1,40 +1,47 @@
-import csv
+import pymysql
 
 
 class LoadInformationFromDatabase:
-
-    @staticmethod
-    def get_students():
+    
+    def __init__(self):
+        self.db = pymysql.connect(
+            host='localhost',
+            user='root',
+            password='root',
+            database='ssis_db'
+        )
+        
+        self.cursor = self.db.cursor()
+    
+    def get_students(self):
         students_data = []
 
-        with open("../databases/students.csv", 'r') as from_students_csv:
-            reader = csv.reader(from_students_csv)
+        self.cursor.execute("SELECT * FROM students")
+        result = self.cursor.fetchall()
 
-            for index, row in enumerate(reader):
-                students_data.append(row)
+        for row in result:
+            students_data.append(list(row))
 
         return students_data
 
-    @staticmethod
-    def get_programs():
+    def get_programs(self):
         programs_data = []
 
-        with open("../databases/programs.csv", 'r') as from_programs_csv:
-            reader = csv.reader(from_programs_csv)
+        self.cursor.execute("SELECT * FROM programs")
+        result = self.cursor.fetchall()
 
-            for row in reader:
-                programs_data.append(row)
+        for row in result:
+            programs_data.append(list(row))
 
         return programs_data
 
-    @staticmethod
-    def get_colleges():
+    def get_colleges(self):
         colleges_data = []
 
-        with open("../databases/colleges.csv", 'r') as from_colleges_csv:
-            reader = csv.reader(from_colleges_csv)
+        self.cursor.execute("SELECT * FROM colleges")
+        result = self.cursor.fetchall()
 
-            for row in reader:
-                colleges_data.append(row)
+        for row in result:
+            colleges_data.append(list(row))
 
         return colleges_data
