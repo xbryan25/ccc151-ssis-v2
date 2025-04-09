c3770cc6d1389ef014fe506fc018603fea2bd24c
Bryan <bryanagan25@gmail.com>
2025-04-09 16:45:46 +0800

    Bugfix: Fix context menu persisting even after clicking one of its buttons

--- src/application/context_menu_setup.py
+++ src/application/context_menu_setup.py
@@ -43,15 +43,23 @@ class ContextMenuSetup:
 
         if self.entity_type != "college" or (self.entity_type == "college" and len(selected_rows) == 1):
             edit_action = menu.addAction("Edit")
-            edit_action.triggered.connect(lambda: self.delete_or_edit_entity("edit"))
+            edit_action.setData("edit")
+            edit_action.triggered.connect(self.edit_entity)
 
         delete_action = menu.addAction("Delete")
-        delete_action.triggered.connect(lambda: self.delete_or_edit_entity("delete"))
+        delete_action.setData("edit")
+        delete_action.triggered.connect(self.delete_entity)
 
         # Execute menu and get selected action
         menu.exec(self.table_view.viewport().mapToGlobal(pos))
 
-    def delete_or_edit_entity(self, delete_or_edit):
+    # Can't combine the two methods because lambda functions can't be used because of weird
+    # event issues that will affect the QMenu
+
+    # It delays execution, and when used inside a QMenu.exec() call, it runs after the menu closes, which eventually
+    # results in the QMenu popping up again
+
+    def delete_entity(self):
 
         selected_indexes = self.table_view.selectionModel().selectedIndexes()
 
@@ -60,14 +68,22 @@ class ContextMenuSetup:
 
         identifiers = self.current_model.get_identifiers_of_selected_rows(selected_rows)
 
-        if delete_or_edit == "delete":
-            delete_entity_handler = DeleteEntityHandler(selected_rows, identifiers, self.current_model, self.table_view,
-                                                        self.entity_type, self.save_changes_button,
-                                                        self.undo_all_changes_button)
-            delete_entity_handler.delete_entities()
-
-        elif delete_or_edit == "edit":
-            edit_entity_handler = EditEntityHandler(selected_rows, identifiers, self.current_model, self.table_view,
+        delete_entity_handler = DeleteEntityHandler(selected_rows, identifiers, self.current_model, self.table_view,
                                                     self.entity_type, self.save_changes_button,
-                                                    self.undo_all_changes_button, self.reset_item_delegates_func)
-            edit_entity_handler.edit_entities()
+                                                    self.undo_all_changes_button)
+        delete_entity_handler.delete_entities()
+
+    def edit_entity(self):
+        selected_indexes = self.table_view.selectionModel().selectedIndexes()
+
+        selected_rows = list(set(index.row() for index in selected_indexes))
+        selected_rows.sort()
+
+        identifiers = self.current_model.get_identifiers_of_selected_rows(selected_rows)
+
+        edit_entity_handler = EditEntityHandler(selected_rows, identifiers, self.current_model, self.table_view,
+                                                self.entity_type, self.save_changes_button,
+                                                self.undo_all_changes_button, self.reset_item_delegates_func)
+        edit_entity_handler.edit_entities()
+
+
